700376c19f84bd862710db2cac2ee4e352243c5a
Isaias123Bv <114431930+Isaias123Bv@users.noreply.github.com>
2022-09-30 22:14:42 -0500

    Add files via upload

---
+++ index.php
@@ -0,0 +1,345 @@
+<!DOCTYPE html>
+<html lang="en">
+<head>
+	<meta charset="UTF-8">
+	<meta name="viewport" content="width=device-width, initial-scale=1.0">
+	<title>CV</title>
+	<link href="css/bootstrap.min.css" rel="stylesheet">
+	<link href="https://fonts.googleapis.com/icon?family=Material+Icons"
+	rel="stylesheet">
+	<link rel="stylesheet" href="https://fonts.googleapis.com/css2?family=Material+Symbols+Outlined:opsz,wght,FILL,GRAD@20..48,100..700,0..1,-50..200" />
+	<script src="https://kit.fontawesome.com/be0be6fea0.js" crossorigin="anonymous"></script>
+
+
+</head>
+<body>
+
+	<nav class="navbar fixed-top navbar-expand-lg navbar-light bg-light">
+		<div class="container-fluid">
+			<a class="navbar-brand" href="#">Sobre mí</a>
+			<button class="navbar-toggler" type="button" data-bs-toggle="collapse" data-bs-target="#navbarNav" aria-controls="navbarNav" aria-expanded="false" aria-label="Toggle navigation">
+				<span class="navbar-toggler-icon"></span>
+			</button>
+
+			<div class="navbar-nav mr-auto" id="navbarNav">
+				<ul class="navbar-nav" >
+					<li class="nav-item">
+						<a class="nav-link active" aria-current="page" href="#estudios">Estudios</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#portafolio">Portafolio</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#intereses">Intereses</a>
+					</li>
+					<li class="nav-item">
+						<a class="nav-link" href="#contacto">Contacto</a>
+					</li>
+				</ul>
+			</div>
+		</div>
+	</nav>
+	<br>
+
+	<div class="container" style="padding-top: 50px">
+		<div class="row align-items-start">
+			<div class="col">
+				<h1 align="center" class="card-subtitle mb-2 text-muted">Isaias Percino Rosas</h1>
+				<p>Soy una persona que se se esmera en aprender, que trata de superarse asi mismo, la cual tiene ciertas dificultades pero aun asi sigue tratando de salir adelante.</p>
+			</div>
+
+
+			<div class="col" >
+				<img  width="40%" src="imagenes/mi.jpg" class="img-fluid rounded-start" alt="...">
+			</div>
+		</div>
+		<br>
+
+		<div id="estudios" class="container-fluid" style="background-color:#F0FCFF; color:white; padding-top:60px">
+			<div class="row text-center">
+					<h1 class="display-1" align="center" style="background-color:#F64C72; color: black;">Estudios</h1>
+
+
+				<div class="container">
+					<div class="row align-items-start">
+						<div class="col">
+
+
+							<table class="table">
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #F64C72;" ></th>
+										<th scope="col" style="background-color: #F64C72;">Institución educativa</th>
+
+									</tr>
+								</thead>
+								<tbody>
+									<tr align="left">
+										<th scope="row">1</th>
+										<td>Emiliano Zapata Salazar</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">2</th>
+										<td>Bachillerato Oficial Tehuiloyocan</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">3</th>
+										<td colspan="2">Universidad Tecnologica de Huejotzingo</td>
+									</tr>
+								</tbody>
+
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #F64C72";></th>
+										<th scope="col" style="background-color: #F64C72";>Cursos</th>
+									</tr>
+								</thead>
+
+								<tbody>
+									<tr align="left">
+										<th scope="row">1</th>
+										<td>Certificado Habilidades Digitales</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">2</th>
+										<td>Ciber Seguridad</td>
+									</tr>
+
+									<tr align="left">
+										<th scope="row">3</th>
+										<td colspan="2">
+Curso de Java SE Orientado a Objetos</td>
+									</tr>
+
+
+									<tr align="left">
+										<th scope="row">4</th>
+										<td colspan="2">Scrum Fundamentals Certified</td>
+									</tr>
+								</tbody>
+  				</div>
+  			</div>
+
+								<thead>
+									<tr>
+										<th scope="col" style="background-color: #F64C72;"></th>
+										<th scope="col" style="background-color:#F64C72;">Conocimientos y Habilidades</th>
+									</tr>
+								</thead>
+
+
+								<tbody>
+									<tr align="left">
+										<th scope="row"></th>
+										<td>
+											<ul>
+												<li type="square"> Word</li>
+												<li type="square"> Excel</li>
+												<br>
+												<li> Diseñador Gráfico</li>
+												<li>Jugador de Minecraft Tecnico</li>
+												<li> Jugar profesional de Clash  Royale</li>
+											</ul>
+										</td>
+									</tr>
+								</tbody>
+							</table>
+						</div>
+
+						<div class="col">
+							<div id="carouselExampleCaptions" class="carousel slide" data-bs-ride="carousel" >
+								<div class="carousel-indicators">
+									<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="0" class="active" aria-current="true" aria-label="Slide 1"></button>
+									<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="1" aria-label="Slide 2"></button>
+									<button type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide-to="2" aria-label="Slide 3"></button>
+								</div>
+								<div class="carousel-inner">
+									<div class="carousel-item active">
+										<img src="imagenes/foto1.jpeg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="imagenes/foto2.jpeg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+									<div class="carousel-item">
+										<img src="imagenes/foto3.jpeg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+										<div class="carousel-item">
+										<img src="imagenes/foto4.jpeg" class="d-block w-100" alt="...">
+										<div class="carousel-caption d-none d-md-block">
+										</div>
+									</div>
+								<button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="prev">
+									<span class="carousel-control-prev-icon" aria-hidden="true"></span>
+									<span class="visually-hidden">Previous</span>
+								</button>
+								<button class="carousel-control-next" type="button" data-bs-target="#carouselExampleCaptions" data-bs-slide="next">
+									<span class="carousel-control-next-icon" aria-hidden="true"></span>
+									<span class="visually-hidden">Next</span>
+								</button>
+							</div>
+						</div>
+					</div>
+				</div>
+			</div>
+		</div>
+	</div>
+	<br>
+	<br>
+
+
+	<div id="portafolio" class="container" style="padding-top: 50px">
+		<div class="row text-center">
+			<h1 class="display-1" align="center" style="background-color:#F64C72; color: black;">Portafolio</h1>
+  <div class="row align-items-start">
+
+    <div class="col">
+      <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagenes/foto5.jpeg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Primer cuatrimestre</h5>
+        <p class="card-text">La empresa TECH INNOVATION está expandiéndose ofreciendo múltiples servicios en tecnología como en la venta de artículos de tecnología, mantenimiento correctivo y preventivo en PC, venta de los artículos en una página web, para poder implementar sus servicios contrato a personal capacitado para desarrollar el proyecto de manera adecuada. </p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+    </div>
+    <div class="col">
+      <div class="card mb-3" style="max-width: 540px;">
+  <div class="row g-0">
+    <div class="col-md-4">
+      <img src="imagenes/foto5.jpeg" class="img-fluid rounded-start" alt="...">
+    </div>
+    <div class="col-md-8">
+      <div class="card-body">
+        <h5 class="card-title">Segundo cuatrimestre</h5>
+        <p class="card-text">El servicio que ofrecemos es una app donde los usuarios les permite escuchar música a través desde su celular, como bien sabemos décadas atrás había otras ,maneras de reproducir música.</p>
+        
+      </div>
+    </div>
+  </div>
+</div>
+</div>
+<br>
+
+<div id="carouselExampleFade" class="carousel slide carousel-fade" data-bs-ride="carousel">
+  <div class="carousel-inner">
+    <div class="carousel-item active">
+      <img src="imagenes/foto6.jpg" class="d-block w-100" alt="..." width="640" height="550">
+    </div>
+    <div class="carousel-item">
+      <img src="imagenes/foto7.jpg" class="d-block w-100" alt="..." width="640" height="550">
+    </div>
+    <div class="carousel-item">
+      <img src="imagenes/foto8.jpg" class="d-block w-100" alt="..." width="640" height="550">
+    </div>
+  </div>
+  <button class="carousel-control-prev" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="prev">
+    <span class="carousel-control-prev-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Previous</span>
+  </button>
+  <button class="carousel-control-next" type="button" data-bs-target="#carouselExampleFade" data-bs-slide="next">
+    <span class="carousel-control-next-icon" aria-hidden="true"></span>
+    <span class="visually-hidden">Next</span>
+  </button>
+</div>
+</div>
+</div>
+<br>
+
+<div class="row" id="intereses" align="center">
+	<div class="col-12">
+			<h1 class="display-1" align="center" style="background-color:#F64C72; color: black;">Intereses</h1>
+
+			<div class="row">
+			<div class="col-4">
+				<div class="card" style="width: 18rem;">
+		<span class="material-symbols-outlined"
+		style="font-size:48px">
+		stadia_controller
+		</span>
+  				<div class="card-body">
+  					<h4>Jugar video juegos:</h4>
+   				 <p class="card-text">Soy alguien apasionado a los videojuegos, puedo pasar horas jugando, ya que la moyoria de juegos los tengo en ingles y me ayuda aprender palabras.</p>
+ 				 </div>
+				</div>
+			</div>
+
+			<div class="col-4">
+				<div class="card" style="width: 18rem;">
+ 			 	<span class="material-icons" style="font-size:48px">headphones</span>
+				<h4>Escuchar musica:</h4>
+  				<div class="card-body">
+   				 <p class="card-text">Me encanta escuchar musica de forma aislada ya que me siento relajado conmigo mismo.</p>
+ 				 </div>
+				</div>
+			</div>
+
+			<div class="col-4">
+				<div class="card" style="width: 18rem;">
+					<span class="material-symbols-outlined" style="font-size:48px">
+					pets
+					</span>
+
+				<h4>Cuidar a mi mascota:</h4>
+  				<div class="card-body">
+   				 <p class="card-text">Me gusta cuidar a mi gata, llamada vakypandi tiene 3 meses conmigo y la cuidado bastante bien<3.</p>
+ 				 </div>
+				</div>
+			</div>
+		</div>
+	</div>
+</div>
+				<div id="contacto" class="container-fluid" style="background-color:black; color:white;">
+				<div class="row text-center">
+					<h1 class="display-1">Contacto</h1>
+
+				</div>
+				<br>
+				<div class="row">
+					<div class="col-md-6 text-center">
+						
+						<h3>REDES SOCIALES</h3>
+						<H1>
+					<a href="https://www.facebook.com/isaias.bv.75"><i class="fa-brands fa-facebook"></i></a> 
+					<a href="https://instagram.com/"></a><i class="fa-brands fa-instagram"></i><br>	
+					 
+					 <br>
+
+					</div>
+					<div class="col-md-6 text-center">
+						
+						<span class="material-icons">location_on </span> San Pedro Cholula-Puebla <br>
+						<span class="material-icons">phone </span> 2211107257 <br>
+						<span class="material-icons">mail </span> 3521110250@uth.edu.mx 
+						
+						
+					</div>
+				</H1>
+			</div>
+		</div>
+		<br>
+					<p align="center">Derechos Reservados 2022</p>
+
+
+
+
+
+
+	<script src="js/bootstrap.bundle.min.js"></script>
+
+</body>
+</html>
